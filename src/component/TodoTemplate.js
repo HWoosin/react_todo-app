6e45bc2fe953da09c900e42335253058a7ec1301
HWoosin <ws1340cc@naver.com>
2023-06-14 11:15:46 +0900

    버튼을 눌렸을때input이 생겨나는 이벤트 처리 230614

--- src/component/TodoTemplate.js
+++ src/component/TodoTemplate.js
@@ -6,6 +6,9 @@ import TodoInput from './TodoInput'
 import './scss/TodoTemplate.scss';
 
 const TodoTemplate = () => {
+
+  //서버에 할일 목록(json)을 요청(fetch)해서 받아와야함.
+
   return (
     <div className='TodoTemplate'>
         <TodoHeader/>

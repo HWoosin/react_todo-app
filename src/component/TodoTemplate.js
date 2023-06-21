a21a49948422f8224c4198ce355d0f6caa526995
HWoosin <ws1340cc@naver.com>
2023-06-21 16:08:16 +0900

    리액트와 todoAPI 연동 230621

--- src/component/TodoTemplate.js
+++ src/component/TodoTemplate.js
@@ -8,6 +8,7 @@ import './scss/TodoTemplate.scss';
 const TodoTemplate = () => {
 
   //서버에 할일 목록(json)을 요청(fetch)해서 받아와야함.
+  const API_BASE_URL = 'http://localhost:8181/api/todos';
 
   //todos 배열을 상태 관리
   const [todos, setTodos] = useState([
@@ -47,9 +48,10 @@ const TodoTemplate = () => {
   const addTodo = todoText => {
     // console.log('할 일 정보: ', todoText);
     const newTodo={
-      id: makeNewId(),
-      title: todoText,
-      done: false
+      // id: makeNewId(),
+      // title: todoText,
+      // done: false
+      title : todoText
     };
     // todos.push(newTodo); (x) ->useState
     //리액트의 상태변수는 무조건 setter를 통해서만
@@ -61,7 +63,17 @@ const TodoTemplate = () => {
     // copyTodos.push(newTodo);
     // setTodos(todos.concat([newTodo]));
 
-    setTodos([...todos, newTodo]);
+    fetch(API_BASE_URL, {
+      method : 'POST',
+      headers : {'content-type' : 'application/json'},
+      body: JSON.stringify(newTodo)
+    })
+    .then(res => res.json())
+    .then(json =>{
+      setTodos(json.todos);
+    })
+
+    // setTodos([...todos, newTodo]);
   }
   
   //할 일 삭제 처리 함수
@@ -81,11 +93,21 @@ const TodoTemplate = () => {
 
     //주어진 배열의 값들을 순회하여 조건에 맞는 요소들만 모아서
     //새로운 배열로 리턴해 주는 함수.
-    setTodos(todos.filter(todo => todo.id !== id));
+
+
+    fetch(`${API_BASE_URL}/${id}`,{
+      method : 'DELETE'
+    })
+    .then(res => res.json())
+    .then(json=>{
+      setTodos(json.todos)
+    })
+
+    // setTodos(todos.filter(todo => todo.id !== id));
   }
 
   //할 일 체크 처리 함수
-  const checkTodo = id =>{
+  const checkTodo = (id, done) =>{
     // console.log(`체크 대상 id:${id}`);
     // const copyTodos = [...todos];
     // for(let cTodo of copyTodos){
@@ -93,10 +115,24 @@ const TodoTemplate = () => {
     //     cTodo.done = !cTodo.done;
     //   }
     // }
+    let check ={
+      'done' : !done,
+      'id' : id
+    }
+    
 
     // setTodos(copyTodos);
+    fetch(API_BASE_URL,{
+      method : 'PUT',
+      headers : {'content-type' : 'application/json'},
+      body: JSON.stringify(check)
+    })
+    .then(res => res.json())
+    .then(json=>{
+      setTodos(json.todos)
+    });
     
-    setTodos(todos.map(todo => todo.id === id ? {...todo, 'done':!todo.done} : todo));
+    // setTodos(todos.map(todo => todo.id === id ? {...todo, 'done':!todo.done} : todo));
 
   }
 
@@ -106,8 +142,14 @@ const TodoTemplate = () => {
   }
 
   useEffect(()=>{
-    console.log(todos);
-  },[todos]);
+    //페이지가 렌더링 됨과 동시에 할 일 목록을 요청해서 뿌려 주겠다.
+    fetch(API_BASE_URL)
+    .then(res => res.json())
+    .then(json => {
+      console.log(json.todos);
+      setTodos(json.todos);
+    })
+  },[]);
 
   return (
     <div className='TodoTemplate'>

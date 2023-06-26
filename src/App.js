11672ce46fdd510654c0619840ec2d6641c1548e
HWoosin <ws1340cc@naver.com>
2023-06-26 16:49:43 +0900

    토큰을 사용한 로그인을 구현했습니다. 목록화면을 구성중입니다. 230626

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import TodoTemplate from './component/todo/TodoTemplate';
 import Join from './component/user/Join';
 import Login from './component/user/Login';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {//일부러 url을 표시해주기 위해 사용하는 Route, 그럼 해당되는 곳에 Link를 줘야함
   return (
     <>

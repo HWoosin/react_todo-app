47701779f7fb45f26a8d8a258af38b3b3a9ac230
HWoosin <ws1340cc@naver.com>
2023-07-05 10:16:35 +0900

    일부 오류 수정 완료 프로필사진 불러오기 230705

--- src/component/user/Login.js
+++ src/component/user/Login.js
@@ -9,94 +9,108 @@ import CustomSnackBar from '../layout/CustomSnackBar';
 
 const Login = () => {
 
+    console.log('Login Component Rendering!');
+
     const redirection = useNavigate();
 
-    //AuthContext에서 onLogin 함수를 가져옵니다.
+    console.log('bring onLogin in AuthContext!');
+    // AuthContext에서 onLogin 함수를 가져옵니다.
     const { onLogin, isLoggedIn } = useContext(AuthContext);
+    console.log('isLoggedIn: ', isLoggedIn);
 
     const [open, setOpen] = useState(false);
 
-    useEffect(()=>{
-        if(isLoggedIn){
+    useEffect(() => {
+        console.log('Login useEffect Called!');
+        if (localStorage.getItem('ACCESS_TOKEN')) {
+            console.log('isLoggedIn True!');
             setOpen(true);
-            setTimeout(()=>{
+            setTimeout(() => {
                 redirection('/');
             }, 3000);
         }
-    },[isLoggedIn, redirection]);
+    }, []);
 
     const REQUEST_URL = BASE + USER + '/signin';
 
     //서버에 비동기 로그인 요청
-    //함수 앞에 async를 붙이면 해당 함수는 프로미스 객체를 바로 리턴한다.
-    const fetchLogin = async() =>{
+    //함수 앞에 async를 붙이면 해당 함수는 프로미스 객체를 바로 리턴합니다.
+    const fetchLogin = async() => {
 
         //사용자가 입력한 이메일, 비밀번호 입력 태그 얻어오기
         const $email = document.getElementById('email');
         const $password = document.getElementById('password');
 
-        //await는 async로 선언된 함수에서만 사용이 가능하다.
-        //await는 프로미스 객체가 처리될 때까지 기다린다.
-        //프로미스 객체의 반환값을 바로 활용할 수 있게 도와준다.
-        //then()을 활용하는 것보다 가독성이 좋고 쓰기도 쉽다.
+        //await는 async로 선언된 함수에서만 사용이 가능합니다.
+        //await는 프로미스 객체가 처리될 때까지 기다립니다. 
+        //프로미스 객체의 반환값을 바로 활용할 수 있게 도와줍니다.
+        //then()을 활용하는 것보다 가독성이 좋고 쓰기도 쉽습니다.
         const res = await fetch(REQUEST_URL, {
-            method:'POST',
-            headers:{'content-type': 'application/json'},
+            method: 'POST',
+            headers: { 'content-type' : 'application/json' },
             body: JSON.stringify({
                 email: $email.value,
                 password: $password.value
-            })
+             })
         });
+        console.log('login fetch complete!');
 
-        if(res.status === 400){
+        if (res.status === 400) {
             const text = await res.text();
             alert(text);
             return;
         }
 
-        const {token, userName, email, role} = await res.json();
+        const { token, userName, email, role } = await res.json();
         // console.log(json);
+      
 
-        //Context API를 사용하여 로그인 상태를 업데이트한다.
+        console.log('setting login data in Context!');
+        // Context API를 사용하여 로그인 상태를 업데이트합니다.
         onLogin(token, userName, role);
+        
         
         //홈으로 리다이렉트
         redirection('/');
 
 
-        // fetch(REQUEST_URL,{
-        //     method:'POST',
-        //     headers:{'content-type': 'application/json'},
+        // fetch(REQUEST_URL, {
+        //    
+        //     headers: { 'content-type' : 'application/json' },
         //     body: JSON.stringify({
         //         email: $email.value,
         //         password: $password.value
         //     })
-        // })//틀리면 TEXT 맞으면 JSON
+        // })
         // .then(res => {
-        //     if(res.status === 400){//가입이 안되어있거나, 비번 틀린 경우
+        //     if(res.status === 400) { // 가입이 안되어있거나, 비번 틀린 경우
         //         return res.text();
         //     }
         //     return res.json();
         // })
         // .then(result => {
-        //     if(typeof result ==='string'){
+        //     if(typeof result === 'string') {
         //         alert(result);
+        //         return;
         //     }
         //     console.log(result);
         // });
     }
 
     //로그인 요청 핸들러
-    const loginHandler = e =>{
+    const loginHandler = e => {
         e.preventDefault();
-        //서버에 로그인 요청 전송
+
+        
+
+        // 서버에 로그인 요청 전송
         fetchLogin();
 
     }
 
     return (
         <>
-            {!isLoggedIn &&
+            {!isLoggedIn && 
             <Container component="main" maxWidth="xs" style={{ margin: "200px auto" }}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
@@ -105,7 +119,7 @@ const Login = () => {
                         </Typography>
                     </Grid>
                 </Grid>
-                <form noValidate onSubmit = {loginHandler}>
+                <form noValidate onSubmit={loginHandler}>
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
                             <TextField
@@ -144,7 +158,7 @@ const Login = () => {
                 </form>
             </Container>
             }
-            <CustomSnackBar
+            <CustomSnackBar 
                 open={open}
             />
         </>
@@ -152,4 +166,4 @@ const Login = () => {
 
 }
 
-export default Login
+export default Login;
